277ed873a5a7927bc91cf3ddc41964ab2ade6e85
apple <603949699@qq.com>
2019-03-06 21:42:39 +0800

    init

---
+++ src/main/java/com/xugx/github/mybatis/debug/MybatisDebugApplication.java
@@ -0,0 +1,13 @@
+package com.xugx.github.mybatis.debug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisDebugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDebugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xugx/github/mybatis/debug/MybatisDebugApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xugx.github.mybatis.debug;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybatisDebugApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
